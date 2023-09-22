cc36de0d7447b6a2980069a60cb387fe290f11e7
rjgallac <rjgallac@hotmail.com>
2023-09-22 20:02:13 +0100

    kinesis and localstack demo

--- src/main/java/com/example/demo/config/AwsConfig.java
+++ src/main/java/com/example/demo/config/AwsConfig.java
@@ -1,11 +1,14 @@
 package com.example.demo.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 
 import static com.amazonaws.services.dynamodbv2.streamsadapter.model.RecordObjectMapper.DYNAMODB;
 
@@ -14,8 +17,21 @@ public class AwsConfig {
 
     @Bean
     public AmazonDynamoDB dynamoDBClient() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials("dummy", "dummy");
+
         return  AmazonDynamoDBClientBuilder.standard()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", "us-east-1"))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+    }
+
+    @Bean
+    public AmazonDynamoDBAsync dynamoDBClientAsync() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials("dummy", "dummy");
+
+        return AmazonDynamoDBAsyncClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", "us-east-1"))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
     }
 }
